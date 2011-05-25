0c8b3ca9aba58c13c728f0820664524c9e2ffb55
Mike Seaby <mike@pixelspring.co.uk>
2011-05-25 17:16:04 +0100

    Device Rotation Startup for iPad & iPhone added in comments (Floz)

--- Classes/ObjC_MixAppDelegate.m
+++ Classes/ObjC_MixAppDelegate.m
@@ -79,7 +79,7 @@ extern BOOL _mainWebViewLoaded;
  [window addSubview:splashView];
  [window bringSubviewToFront:splashView];
  [UIView beginAnimations:nil context:nil];
- [UIView setAnimationDuration:3.0];
+ [UIView setAnimationDuration:2.0];
  [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:window cache:YES];
  [UIView setAnimationDelegate:self]; 
  [UIView setAnimationDidStopSelector:@selector(startupAnimationDone:finished:context:)];
@@ -91,11 +91,11 @@ extern BOOL _mainWebViewLoaded;
  {
  // The device is an iPhone or iPod touch.
  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0, 320, 480)];
- splashView.image = [UIImage imageNamed:@"Default@2x.png"];
+ splashView.image = [UIImage imageNamed:@"Default.png"];
  [window addSubview:splashView];
  [window bringSubviewToFront:splashView];
  [UIView beginAnimations:nil context:nil];
- [UIView setAnimationDuration:3.0];
+ [UIView setAnimationDuration:2.0];
  [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:window cache:YES];
  [UIView setAnimationDelegate:self]; 
  [UIView setAnimationDidStopSelector:@selector(startupAnimationDone:finished:context:)];
@@ -104,3 +104,87 @@ extern BOOL _mainWebViewLoaded;
  [UIView commitAnimations];
  }
  */
+ 
+ 
+ 
+ 
+//****************************************************************
+// Floz's Device-rotation-compatible startup animation
+// (device only, NOT for simulator)
+//**************************************************************** 
+
+  
+  /*
+  BOOL iPad = NO;
+  #ifdef UI_USER_INTERFACE_IDIOM
+  iPad = (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad);
+  #endif
+  
+  if (!iPad) {
+  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0, 320, 480)];  
+  splashView.image = [UIImage imageNamed:@"Default.png"];  
+  [window addSubview:splashView];  
+  [window bringSubviewToFront:splashView];  
+  [UIView beginAnimations:nil context:nil];  
+  [UIView setAnimationDuration:.5];  
+  [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+  [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:window cache:YES];  
+  [UIView setAnimationDelegate:self];   
+  [UIView setAnimationDidStopSelector:@selector(startupAnimationDone:finished:context:)];  
+  splashView.alpha = 0.0;  
+  splashView.frame = CGRectMake(0-60, 0-90, 320+2*60, 480+2*90);  
+  [UIView commitAnimations];
+  } else { // iPads   
+  
+  [[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+  
+  int orientation = [[UIDevice currentDevice] orientation];
+  int y;
+  
+  switch (orientation) {
+  case UIDeviceOrientationPortrait:
+  y = 20;
+  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(0,y, 768, 1004)];
+  splashView.image = [UIImage imageNamed:@"Default-Portrait~ipad.png"];
+  //            NSLog(@"UIDeviceOrientationPortrait");
+  break;
+  
+  case UIDeviceOrientationPortraitUpsideDown:
+  y = 0;
+  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(0,y, 768, 1004)];
+  splashView.image = [UIImage imageNamed:@"Default-Portrait~ipad.png"];
+  splashView.transform = CGAffineTransformMakeRotation(M_PI);
+  //            NSLog(@"UIDeviceOrientationPortraitUpsideDown");
+  break;
+  
+  case UIDeviceOrientationLandscapeLeft:
+  y = 0;
+  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(-(1024-748)/2+y,(1024-748)/2, 1024, 748)];
+  splashView.image = [UIImage imageNamed:@"Default-Landscape~ipad.png"];
+  splashView.transform = CGAffineTransformMakeRotation(M_PI/2);
+  //            NSLog(@"UIDeviceOrientationLandscapeLeft");
+  break;
+  
+  default: // UIDeviceOrientationLandscapeRight
+  y = 20;
+  splashView = [[UIImageView alloc] initWithFrame:CGRectMake(-(1024-748)/2+y,(1024-748)/2, 1024, 748)];
+  splashView.image = [UIImage imageNamed:@"Default-Landscape~ipad.png"];
+  splashView.transform = CGAffineTransformMakeRotation(3*M_PI/2);
+  //            NSLog(@"UIDeviceOrientationLandscapeRight");
+  }
+  
+  [[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+  
+  [window addSubview:splashView];  
+  [window bringSubviewToFront:splashView];  
+  [UIView beginAnimations:nil context:nil];  
+  [UIView setAnimationDuration:.5];  
+  [UIView setAnimationCurve:UIViewAnimationCurveEaseIn];
+  [UIView setAnimationTransition:UIViewAnimationTransitionNone forView:window cache:YES];  
+  [UIView setAnimationDelegate:self];   
+  [UIView setAnimationDidStopSelector:@selector(startupAnimationDone:finished:context:)];  
+  splashView.alpha = 0.0;  
+  splashView.frame = CGRectMake(0-120, y-160, 768+2*120, 1024+2*160);
+  [UIView commitAnimations];
+  }
+  */
